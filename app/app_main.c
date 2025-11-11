ef83f6082d807a02d3d9724bf15263b72063ed8d
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2025-11-10 16:56:36 -0800

    Small changes to notification debug code

--- app/app_main.c
+++ app/app_main.c
@@ -570,12 +570,20 @@ EXPORT_FUNC APP_UPDATE_DEF(AppUpdate)
 		if (IsKeyboardKeyPressed(&appIn->keyboard, Key_N, true))
 		{
 			DbgLevel level = (DbgLevel)GetRandU8Range(&app->random, DbgLevel_Debug, DbgLevel_Count);
-			u64 duration = GetRandU64Range(&app->random, 1*NUM_MS_PER_SECOND, 10*NUM_MS_PER_SECOND);
-			#if 0
-			AddNotificationToQueue(&app->notificationQueue, level, StrLit("Notification!"));
+			#if 1
+			u32 numRepetitions = GetRandU32Range(&app->random, 1, 10);
+			Str8 testStr = PrintInArenaStr(scratch, "%s Notification!", GetDbgLevelStr(level));
+			NewStrBuffEx(testBuffer, 2048);
+			for (u32 rIndex = 0; rIndex < numRepetitions; rIndex++)
+			{
+				AppendToStrBuffStr(&testBuffer, testStr);
+				AppendToStrBuffChar(&testBuffer, ' ');
+			}
+			NotifyAt(level, 0, testBuffer.str.chars);
 			#else
-			NotifyPrintAt(level, duration, "%s Notification %llu \"%.*s\"!", GetDbgLevelStr(level), duration, StrPrint(app->mapBackTexturePath));
+			NotifyPrintAt(level, 0, "%s Notification \"%.*s\"!", GetDbgLevelStr(level), StrPrint(app->mapBackTexturePath));
 			#endif
+			
 		}
 		
 		UpdateMapView(&app->view, isMouseOverMainViewport, &appIn->mouse, &appIn->keyboard);
